00142654146c4d2d57900bb5c86a04662a184a7a
100gbgaming <anandjyadav922@gmail.com>
2026-03-13 19:33:55 +0530

    Refactor profile update and file upload functions

--- script.js
+++ script.js
@@ -1,85 +1,114 @@
+// UPDATE PROFILE INFO
+
 function updateProfile(){
 
-username.innerText=usernameInput.value
-name.innerText=nameInput.value
-followers.innerText=followersInput.value
-following.innerText=followingInput.value
-bio.innerText=bioInput.value
-reelViews.innerText=reelViewsInput.value
-likes.innerText=likesInput.value
+const usernameInputEl = document.getElementById("usernameInput")
+const nameInputEl = document.getElementById("nameInput")
+const followersInputEl = document.getElementById("followersInput")
+const followingInputEl = document.getElementById("followingInput")
+const bioInputEl = document.getElementById("bioInput")
+const reelViewsInputEl = document.getElementById("reelViewsInput")
+const likesInputEl = document.getElementById("likesInput")
+
+const usernameEl = document.getElementById("username")
+const nameEl = document.getElementById("name")
+const followersEl = document.getElementById("followers")
+const followingEl = document.getElementById("following")
+const bioEl = document.getElementById("bio")
+const reelViewsEl = document.getElementById("reelViews")
+const likesEl = document.getElementById("likes")
+
+if(usernameInputEl.value) usernameEl.innerText = usernameInputEl.value
+if(nameInputEl.value) nameEl.innerText = nameInputEl.value
+if(followersInputEl.value) followersEl.innerText = followersInputEl.value
+if(followingInputEl.value) followingEl.innerText = followingInputEl.value
+if(bioInputEl.value) bioEl.innerText = bioInputEl.value
+if(reelViewsInputEl.value) reelViewsEl.innerText = reelViewsInputEl.value
+if(likesInputEl.value) likesEl.innerText = likesInputEl.value
 
 }
 
-/* PROFILE PHOTO */
 
-profileUpload.onchange=function(e){
+// PROFILE PHOTO UPLOAD
 
-const file=e.target.files[0]
-const reader=new FileReader()
+document.getElementById("profileUpload").addEventListener("change", function(e){
 
-reader.onload=function(){
-profilePic.src=reader.result
-}
+const file = e.target.files[0]
+if(!file) return
 
-reader.readAsDataURL(file)
+const reader = new FileReader()
 
-}
-
-/* ADD POST */
-
-postUpload.onchange=function(e){
-
-const file=e.target.files[0]
-const reader=new FileReader()
-
-reader.onload=function(){
-
-const img=document.createElement("img")
-img.src=reader.result
-
-postsGrid.appendChild(img)
-
-posts.innerText=postsGrid.children.length
-
+reader.onload = function(){
+document.getElementById("profilePic").src = reader.result
 }
 
 reader.readAsDataURL(file)
 
-}
+})
 
-/* ADD STORY */
 
-storyUpload.onchange=function(e){
+// ADD POST IMAGE
 
-const file=e.target.files[0]
-const reader=new FileReader()
+document.getElementById("postUpload").addEventListener("change", function(e){
 
-reader.onload=function(){
+const file = e.target.files[0]
+if(!file) return
 
-const story=document.createElement("div")
-story.className="story"
-story.style.backgroundImage=`url(${reader.result})`
+const reader = new FileReader()
 
-stories.appendChild(story)
+reader.onload = function(){
+
+const img = document.createElement("img")
+img.src = reader.result
+
+document.getElementById("postsGrid").appendChild(img)
+
+const totalPosts = document.getElementById("postsGrid").children.length
+document.getElementById("posts").innerText = totalPosts
 
 }
 
 reader.readAsDataURL(file)
 
+})
+
+
+// ADD STORY
+
+document.getElementById("storyUpload").addEventListener("change", function(e){
+
+const file = e.target.files[0]
+if(!file) return
+
+const reader = new FileReader()
+
+reader.onload = function(){
+
+const story = document.createElement("div")
+story.className = "story"
+story.style.backgroundImage = `url(${reader.result})`
+
+document.getElementById("stories").appendChild(story)
+
 }
 
-/* TABS */
+reader.readAsDataURL(file)
+
+})
+
+
+// TABS
 
 function showPosts(){
 
-postsGrid.style.display="grid"
-reelsPage.style.display="none"
+document.getElementById("postsGrid").style.display = "grid"
+document.getElementById("reelsPage").style.display = "none"
 
 }
 
 function showReels(){
 
-postsGrid.style.display="none"
-reelsPage.style.display="block"
+document.getElementById("postsGrid").style.display = "none"
+document.getElementById("reelsPage").style.display = "block"
 
 }
